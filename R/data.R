4d3c726cd5d412ac03584c2f004b0ee23aed3338
Ernest Guevarra <ernest@guevarra.io>
2024-02-10 07:48:17 +0000

    add reference tables for linearized mms

--- R/data.R
+++ R/data.R
@@ -21,3 +21,64 @@
 #' @source https://icd.who.int/docs/icd-api/SupportedClassifications/
 #'
 "icd_versions"
+
+
+#'
+#' ICD-11 Mortality and Morbidity Linearization Outputs
+#'
+#' @format A data frame with 21 columns and 36753 rows:
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *Foundation URI* | Foundation URI |
+#' | *Linearization (release) URI* | Linearization release URI |
+#' | *Code* | ICD-11 Code |
+#' | *BlockId* | Block identifier |
+#' | *Title* | Title |
+#' | *ClassKind* | Class |
+#' | *DepthInKind* | Depth |
+#' | *IsResidual* | Is residual? |
+#' | *PrimaryLocation* | Primary location |
+#' | *ChapterNo* | Chapter number |
+#' | *BrowserLink* | Browser link |
+#' | *iCatLink* | Category link |
+#' | *isLeaf* | Is leaf? |
+#' | *noOfNonResidualChildren* | Number of non-residual children |
+#' | *Primary tabulation* | Primary tabulation |
+#' | *Version* | Date and time linearization was produced |
+#'
+#' @examples
+#' icd11_linearization_mms
+#'
+#' @source https://icd.who.int/dev11/Downloads/Download?fileName=LinearizationMiniOutput-MMS-en.zip
+#'
+"icd11_linearization_mms"
+
+
+#'
+#' ICD-11 Mortality and Morbidity Simple Table Outputs
+#'
+#' @format A data frame with 18 columns and 36044 rows:
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *Foundation URI* | Unique identifier for the entity that will not change |
+#' | *Linearization (release) URI* | Unique identifier for this version of the classification. It includes the linearization name such as MMS and minor version identifier such as 2018 in it |
+#' | *Code* | ICD-11 code for the entity. Note that the groupings do not have a code. |
+#' | *BlockId* | Identifier for high level groupings that do not bear a code |
+#' | *Title* | Title of the entity |
+#' | *ClassKind* | One of the three (chapter, block, category); Chapter is top level classification entities; Blocks are high level groupings that do not bear a code; Categories are entities that has a code |
+#' | *DepthInKind* | Depth of within the Class Kind. For example, a category with depthinkind=2 means it is category whose parent is also a category but grand parent is not |
+#' | *IsResidual* | true if the entity is a residual category (i.e. other specified or unspecified categories) |
+#' | *ChapterNo* | The chapter that the entity is in |
+#' | *BrowserLink* | Direct link to this entity in ICD-11 browser |
+#' | *IsLeaf* | true if this entity does not have any children |
+#' | *Primary Tabulation* | |
+#' | *GroupingX* | Groupings that the entity is included in |
+#'
+#' @examples
+#' icd11_simple_table_mms
+#'
+#' @source https://icdcdn.who.int/static/releasefiles/2024-01/SimpleTabulation-ICD-11-MMS-en.zip
+#'
+"icd11_simple_table_mms"
---
+++ data-raw/icd_linearization_tables.R
@@ -0,0 +1,62 @@
+# Download and process ICD-11 linearizations -----------------------------------
+
+## Load libraries ----
+library(openxlsx2)
+
+
+## Download data ----
+
+### Linearization MMS from ICD-11 download page ----
+### https://icd.who.int/dev11/downloads
+
+download_url <- "https://icd.who.int/dev11/Downloads/Download?fileName=LinearizationMiniOutput-MMS-en.zip"
+
+download.file(
+  url = download_url,
+  destfile = "data-raw/linear-mms.zip"
+)
+
+unzip(
+  zipfile = "data-raw/linear-mms.zip",
+  overwrite = TRUE,
+  exdir = "data-raw/linear-mms"
+)
+
+icd11_linearization_mms <- read_xlsx(
+  file = "data-raw/linear-mms/LinearizationMiniOutput-MMS-en.xlsx",
+  sheet = 1, data_only = TRUE
+)
+
+header_values <- names(icd11_linearization_mms)[ncol(icd11_linearization_mms)] |>
+  stringr::str_split(pattern = ":", simplify = TRUE)
+
+names(icd11_linearization_mms)[ncol(icd11_linearization_mms)] <- header_values |>
+  (\(x) x[1])()
+
+icd11_linearization_mms$Version <- header_values |>
+  (\(x) x[2:3])() |>
+  paste(collapse = ":")
+
+usethis::use_data(icd11_linearization_mms, overwrite = TRUE, compress = "xz")
+
+### Simple Table MMS ----
+
+download_url <- "https://icdcdn.who.int/static/releasefiles/2024-01/SimpleTabulation-ICD-11-MMS-en.zip"
+
+download.file(
+  url = download_url,
+  destfile = "data-raw/simple-tab-icd11-mms.zip"
+)
+
+unzip(
+  zipfile = "data-raw/simple-tab-icd11-mms.zip",
+  overwrite = TRUE,
+  exdir = "data-raw/simple-tab-icd11-mms"
+)
+
+icd11_simple_table_mms <- read_xlsx(
+  file = "data-raw/simple-tab-icd11-mms/SimpleTabulation-ICD-11-MMS-en.xlsx",
+  sheet = 1, data_only = TRUE
+)
+
+usethis::use_data(icd11_simple_table_mms, overwrite = TRUE, compress = "xz")
