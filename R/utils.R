b2ad8e5db4e84099e544bf706c26f68243b8e754
Ernest Guevarra <ernest@guevarra.io>
2024-02-15 13:15:30 +0000

    function to get release IDs; fix #29 and fix #31

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,31 @@
 #' Utility functions for retrieving ICD parameters and definitions
 #'
 #' @param icd A character string of available ICD classifications. Currently,
-#'   this can be either "icd10" or "icd11". If NULL (default), all ICD
+#'   this can be either "icd10" or "icd11". Default is "icd11". If NULL, all ICD
 #'   classifications are used (currently both "icd10" and "icd11").
 #'
-#' @return A vector of
+#' @return A tibble for ICD classification values and their corresponding
+#'   release identifiers
 #'
+#' @examples
+#' icd_get_releases()
 #'
+#' @rdname icd_utils
+#' @export
+#'
+
+icd_get_releases <- function(icd = c("icd11", "icd10")) {
+  ## Check and get value for icd ----
+  if (!is.null(icd)) {
+    icd <- match.arg(icd)
+  } else {
+    icd <- c("icd11", "icd10")
+  }
+
+  ## Get full ICD name ----
+  icd <- ifelse(icd == "icd10", "ICD-10", "ICD-11")
+
+  ## Get release identifiers for requested classification versions ----
+  codigo::icd_versions[codigo::icd_versions$Classification %in% icd, ] |>
+    subset(select = c("Classification", "Release ID"))
+}
