66c864b5723dc25afca0431aee840b9508e838bc
Ernest Guevarra <ernest@guevarra.io>
2024-02-16 06:55:35 +0000

    test checks within search function

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -10,6 +10,15 @@ testthat::expect_type(test_search_default, "list")
 ## Test no message ----
 testthat::expect_no_message(icd_search_foundation(q = "cholera", verbose = FALSE))
 
+## Test list output ----
 test_search_list <- icd_search_foundation(q = "cholera", tabular = FALSE)
 
 testthat::expect_type(test_search_list, "list")
+
+## Test release specification ----
+test_search_release <- icd_search_foundation(q = "cholera", release = "2024-01")
+testthat::expect_message(icd_search_foundation(q = "cholera", release = "2024-01"))
+testthat::expect_s3_class(test_search_release, "tbl_df")
+testthat::expect_type(test_search_release, "list")
+testthat::expect_error(icd_search_foundation(q = "cholera", release = "2025-01"))
+
