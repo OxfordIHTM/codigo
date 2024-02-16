a9140030e03521446488cbe73f21c07ee6064476
Ernest Guevarra <ernest@guevarra.io>
2024-02-16 07:07:50 +0000

    test chapter and property queries

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -22,3 +22,26 @@ testthat::expect_s3_class(test_search_release, "tbl_df")
 testthat::expect_type(test_search_release, "list")
 testthat::expect_error(icd_search_foundation(q = "cholera", release = "2025-01"))
 
+## Test subtrees query ----
+test_search_subtree <- icd_search_foundation(q = "cholera", subtree = "http://id.who.int/icd/entity/1435254666")
+testthat::expect_message(icd_search_foundation(q = "cholera", subtree = "http://id.who.int/icd/entity/1435254666"))
+testthat::expect_s3_class(test_search_subtree, "tbl_df")
+testthat::expect_type(test_search_subtree, "list")
+
+## Test chapter query ----
+test_search_chapter <- icd_search_foundation(q = "cholera", chapter = "01")
+testthat::expect_message(icd_search_foundation(q = "cholera", chapter = "01"))
+testthat::expect_s3_class(test_search_chapter, "tbl_df")
+testthat::expect_type(test_search_chapter, "list")
+
+## Test properties query ----
+test_search_property <- icd_search_foundation(
+  q = "cholera",
+  properties = c(
+    "Title","Synonym", "NarrowerTerm", "FullySpecifiedName",
+    "Definition", "Exclusion"
+  )
+)
+testthat::expect_s3_class(test_search_property, "tbl_df")
+testthat::expect_type(test_search_property, "list")
+
