82ac933d82d042bb05c94d36a71665dbbea333fb
Ernest Guevarra <ernest@guevarra.io>
2024-02-16 07:33:43 +0000

    update get function tests

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -6,3 +6,8 @@ test_get_foundation_default <- icd_get_foundation()
 testthat::expect_s3_class(test_get_foundation_default, "tbl_df")
 testthat::expect_type(test_get_foundation_default, "list")
 
+## Test release ----
+test_get_foundation_release <- icd_get_foundation(release = "2024-01")
+
+testthat::expect_s3_class(test_get_foundation_release, "tbl_df")
+testthat::expect_type(test_get_foundation_release, "list")
