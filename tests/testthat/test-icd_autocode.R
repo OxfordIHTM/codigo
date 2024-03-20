a6298fe7a0a91186965a5775cce192e428ce9524
Ernest Guevarra <ernest@guevarra.io>
2024-03-20 11:34:19 +0000

    add more autocode tests

--- tests/testthat/test-icd_autocode.R
+++ tests/testthat/test-icd_autocode.R
@@ -1,5 +1,7 @@
 # Test autocode function -------------------------------------------------------
 
+## Autocode foundation ----
+
 testthat::expect_s3_class(
   icd_autocode_foundation("colorectal cancer"),
   "tbl_df"
@@ -7,6 +9,10 @@ testthat::expect_s3_class(
 
 testthat::expect_message(
   icd_autocode_foundation("colorectal cancer")
+)
+
+testthat::expect_error(
+  icd_autocode_foundation("colorectal cancer", release = "2025-01")
 )
 
 testthat::expect_no_message(
@@ -20,5 +26,34 @@ testthat::expect_type(
 
 testthat::expect_s3_class(
   icd_autocode_foundation("colorectal cancer", threshold = 1),
+  "tbl_df"
+)
+
+## Autocode linearisation - mms ----
+
+testthat::expect_s3_class(
+  icd_autocode_mms("colorectal cancer"),
+  "tbl_df"
+)
+
+testthat::expect_message(
+  icd_autocode_mms("colorectal cancer")
+)
+
+testthat::expect_error(
+  icd_autocode_mms("colorectal cancer", release = "2025-01")
+)
+
+testthat::expect_no_message(
+  icd_autocode_mms("colorectal cancer", verbose = FALSE)
+)
+
+testthat::expect_type(
+  icd_autocode_mms("colorectal cancer", tabular = FALSE),
+  "list"
+)
+
+testthat::expect_s3_class(
+  icd_autocode_mms("colorectal cancer", threshold = 1),
   "tbl_df"
 )
