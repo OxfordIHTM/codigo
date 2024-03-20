f4719c5f85dc942ac818a19973712758bb256283
Ernest Guevarra <ernest@guevarra.io>
2024-03-20 12:35:48 +0000

    add more autocode tests

--- tests/testthat/test-icd_autocode.R
+++ tests/testthat/test-icd_autocode.R
@@ -7,6 +7,11 @@ testthat::expect_s3_class(
   "tbl_df"
 )
 
+testthat::expect_s3_class(
+  icd_autocode_foundation("colorectal cancer", release = "2024-01"),
+  "tbl_df"
+)
+
 testthat::expect_message(
   icd_autocode_foundation("colorectal cancer")
 )
