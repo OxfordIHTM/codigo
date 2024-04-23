e745a7a4cb70a1a42688c76a682884a3ea6e6a84
Ernest Guevarra <ernest@guevarra.io>
2024-04-23 23:05:51 +0100

    add test for icd_map function

---
+++ tests/testthat/test-icd_map.R
@@ -0,0 +1,13 @@
+# Tests for icd_map function ---------------------------------------------------
+
+testthat::test_that(
+  "output class/type and structure is as expected", {
+    expect_s3_class(icd_map(code = "A00"), "tbl")
+    expect_length(nrow(icd_map(code = "A00")), 1)
+    expect_named(
+      icd_map(code = "A00"),
+      expected = names(ten_map_to_one_eleven)
+    )
+  }
+)
+
