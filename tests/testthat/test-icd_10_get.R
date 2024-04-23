f68256332458c6d960ca5e2fbe14c4744f4823cb
Ernest Guevarra <ernest@guevarra.io>
2024-04-23 22:48:43 +0100

    add tests for icd_10_get functions

--- tests/testthat/test-icd_10_get.R
+++ tests/testthat/test-icd_10_get.R
@@ -23,3 +23,33 @@ testthat::test_that(
     expect_warning(icd_10_get_chapters(language = "fr"))
   }
 )
+
+
+release_category <- icd_10_get_release_by_category("A00")
+
+testthat::test_that(
+  "release_category is of the right type", {
+    expect_type(release_category, "list")
+  }
+)
+
+
+release_info <- icd_10_get_info(category = "A00")
+
+testthat::test_that(
+  "release_info is of the right type", {
+    expect_type(release_info, "list")
+  }
+)
+
+testthat::test_that(
+  "warning is raised for unavailable language", {
+    expect_warning(icd_10_get_info(category = "A00", language = "zz"))
+  }
+)
+
+testthat::test_that(
+  "error is raised for unavailable release", {
+    expect_error(icd_10_get_info(category = "A00", release = 2020))
+  }
+)
